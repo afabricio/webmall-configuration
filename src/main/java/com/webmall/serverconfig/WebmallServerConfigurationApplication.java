c61c4cdb28dd10bcad8407ecf1023e72c45e9e41
Andre Santos <asantos@kiman.com.br>
2019-01-31 15:22:17 -0200

    First Commit

---
+++ src/main/java/com/webmall/serverconfig/WebmallServerConfigurationApplication.java
@@ -0,0 +1,18 @@
+package com.webmall.serverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class WebmallServerConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmallServerConfigurationApplication.class, args);
+	}
+
+}
+
